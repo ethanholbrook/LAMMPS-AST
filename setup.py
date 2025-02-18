b027517dbda370af1e2a33f1a212ad59c4b7dd9b
Ethan Holbrook <91812978+ethanholbrook@users.noreply.github.com>
2025-02-18 16:22:50 -0500

    Update setup.py
    version 4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lammps-ast",
-    version="0.0.3",
+    version="0.0.4",
     author="Juan C. Verduzco, Ethan W. Holbrook",
     author_email="eholbrooe@purdue.edu",
     description="A LAMMPS script parser and sanitizer using Lark",
