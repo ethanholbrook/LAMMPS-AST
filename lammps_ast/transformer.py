aff637b71673b7184335577178d8c645a9bbdb10
Juan Verduzco <jverduzc@purdue.edu>
2025-02-20 15:21:32 -0500

    Removed Unsupported warnings from Transformer

--- lammps_ast/transformer.py
+++ lammps_ast/transformer.py
@@ -1,17 +1,8 @@
 from lark import Transformer, Tree
-from colorama import Fore, Style
 
-class RemoveNewlinesAndUnsupported(Transformer):
+class RemoveNewlines(Transformer):
     def _NEWLINE(self, token):
         return None
-
-    def UNSUPPORTED_KEYWORD(self, token):
-        print("\t" + f"⚠️ {Fore.YELLOW}Warning:{Style.RESET_ALL} Unsupported keyword '{token.value}' encountered.")
-        return token 
-
-    def UNSUPPORTED_ARGS(self, token):
-        print("\t" + f"⚠️ {Fore.YELLOW}Warning:{Style.RESET_ALL} Unsupported arguments '{token.value}' encountered.")
-        return token 
 
     def __default__(self, data, children, meta):
         filtered_children = [child for child in children if child is not None]
