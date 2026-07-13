05fa329f175a8d50a1ac938ac55cc59791fd3e4c
ethanholbrook <ethanwholbrook@gmail.com>
2026-07-13 11:34:32 -0400

    release v1.0.0

--- lammps_ast/__init__.py
+++ lammps_ast/__init__.py
@@ -3,5 +3,5 @@
 from .parser import list_grammar_versions, parse_to_AST
 from .sanitizer import find_unresolved_variables, sanitize
 
-__version__ = "0.1.91"
+__version__ = "1.0.0"
 __author__ = "Juan C. Verduzco, Ethan W. Holbrook"
